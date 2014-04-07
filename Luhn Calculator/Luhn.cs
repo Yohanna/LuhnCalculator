6fa30ccfe1ae3ddea52a84578a74a72b50d8902d
Yohanna <Yohanna@users.noreply.github.com>
2014-04-06 21:46:38 -0400

    Both overloaded isValid Finished

--- Luhn Calculator/Luhn.cs
+++ Luhn Calculator/Luhn.cs
@@ -8,68 +8,113 @@ namespace Luhn_Calculator
 {
     class Luhn
     {
-        public int checkDigit;
-        public int Number;
-       
+        public int checkDigit { get; private set; }
+        public long Number { get; set; }
+
+
 
         public Luhn()
         {
-            this.Number = -1;
+            Number = -1;
             checkDigit = -1;
         }
 
-        public Luhn(int Number)
+        public Luhn(long Number)
         {
             if (Number < 0)
                 throw new LuhnException("Number can't be negative!");
-            
+
             this.Number = Number;
             checkDigit = -1;
-            isValid(Number);
         }
 
-        public bool isValid(int Number)
+
+        public bool isValid(long Number)
         {
-            //TODO
-            return true;
+            int len = (int)Math.Floor(Math.Log10(Number) + 1);
+
+            List<int> numberList = new List<int>();
+
+            while (Number > 0)
+            {
+                numberList.Add((int)(Number % 10));  // nums are added in reverse order since we need to loop from the right most number      
+                Number /= 10;
+            }
+
+            for (int i = 0; i < len; i++)
+            {
+                if (i % 2 == 1) // i.e. every second digit
+                {
+                    numberList[i] = numberList[i] * 2;
+
+                    if (numberList[i] > 9)
+                        numberList[i] = numberList[i] % 10 + numberList[i] / 10;
+                }
+            }
+
+            return DigitSum(numberList) % 10 == 0 ? true : false;
+        }
+
+        public bool isValid()
+        {
+            long num = Number;
+            int len = (int)Math.Floor(Math.Log10(num) + 1);
+
+
+            List<int> numberList = new List<int>();
+
+            while (num > 0)
+            {
+                numberList.Add((int)(num % 10));  // nums are added in reverse order since we need to loop from the right most number      
+                num /= 10;
+            }
+
+            for (int i = 0; i < len; i++)
+            {
+                if (i % 2 == 1) // i.e. every second digit
+                {
+                    numberList[i] = numberList[i] * 2;
+
+                    if (numberList[i] > 9)
+                        numberList[i] = numberList[i] % 10 + numberList[i] / 10;
+                }
+            }
+
+            return DigitSum(numberList) % 10 == 0 ? true : false;
         }
 
         // Return the next possible Check Digit
         public int NextCheckDigit()
         {
-            if (!isValid(Number))
-                throw new LuhnException("Not a valid number. Enter a valid number first to get the next valid number");
+            //if (!isValid(Number))
+            //    throw new LuhnException("Not a valid number. Enter a valid number first to get the next valid check digit");
 
             if (Number < 0)
-                throw new LuhnException("Number can't be negative!");
+                throw new LuhnException("Please initialize the object before using it!");
 
-            int next=0;
+            int next = 0;
 
-            
+
             //TODO
             return 0;
         }
 
-        public int NextCheckDigit(int Number)
+        public int NextCheckDigit(long Number)
         {
-            if (!isValid(Number))
-                throw new LuhnException("Not a valid number. Enter a valid number first to get the next valid number");
+            //if (!isValid(Number))
+            //    throw new LuhnException("Not a valid number. Enter a valid number first to get the next valid number");
 
             //TODO
 
             return -1;
         }
 
-
-        public int DigitSum(int Number)
+        public int DigitSum(List<int> List)
         {
             int sum = 0;
-            
-            while(Number > 0)
-            {
-                sum += Number % 10;
-                Number /= 10;
-            }
+
+            foreach (int i in List)
+                sum += i;
 
             return sum;
         }
@@ -77,14 +122,10 @@ namespace Luhn_Calculator
 
     }
 
+
     class LuhnException : Exception
     {
-        private string eMessage;
-
         public LuhnException(string message) : base(message) { }
 
     }
-
-
-
 }
